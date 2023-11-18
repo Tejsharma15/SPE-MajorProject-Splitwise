7b0075980c71f72c1c5f6d9146f9ab8f86a756dd
Tejsharma15 <tejsharma815@gmail.com>
2023-11-18 10:17:33 +0530

    Initial commit

---
+++ MiniSplitwise /src/main/java/com/example/MiniSplitwise/MiniSplitwiseApplication.java
@@ -0,0 +1,13 @@
+package com.example.MiniSplitwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniSplitwiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniSplitwiseApplication.class, args);
+	}
+
+}
---
+++ MiniSplitwise /src/test/java/com/example/MiniSplitwise/MiniSplitwiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MiniSplitwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniSplitwiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
