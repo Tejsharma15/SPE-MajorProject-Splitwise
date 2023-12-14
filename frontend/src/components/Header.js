2db67daf173f96c60505e7a9ae57276f89742e2f
Chinthan23 <77500564+Chinthan23@users.noreply.github.com>
2023-12-14 20:37:39 +0530

    expense tracker frontend added

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,18 @@
 import {React,useRef} from 'react';
-import { Heading,Box, Image, Drawer, DrawerBody, DrawerHeader, DrawerOverlay, DrawerContent, DrawerCloseButton, useDisclosure, Button, Spacer, VStack } from '@chakra-ui/react';
-import { HamburgerIcon} from '@chakra-ui/icons'
+import { Heading,Box, Text, Image, Drawer, DrawerBody, DrawerHeader, DrawerOverlay, DrawerContent, DrawerCloseButton, useDisclosure, Button, Spacer, VStack } from '@chakra-ui/react';
+import { HamburgerIcon} from '@chakra-ui/icons';
+import { useDispatch } from 'react-redux';
 import logo from "../logo.png"
 import bg from "../drawerbg.jpg"
 import { Link } from 'react-router-dom';
-function Header() {
+import { logout } from '../store';
+function Header({user}) {
   const { isOpen, onOpen, onClose } = useDisclosure()
   const btnRef = useRef()
+  const dispatch=useDispatch();
+  const handleLogout=()=>{
+    dispatch(logout());
+  }
   return (
     <>
 
@@ -26,19 +32,26 @@ function Header() {
         <DrawerBody>
           <VStack flexDir='column' alignItems='flex-start'>
             <Link onClick={onClose} to="/">Home</Link>
-            <Link onClick={onClose} to="/bills">Bills</Link>
+            <Link onClick={onClose} to="/trackExpense">Track Expenses</Link>
             {/* <Link onClick={onClose} to="/">Contact</Link> */}
           </VStack>
         </DrawerBody>
       </DrawerContent>
     </Drawer>
     <Box display='flex' flexDirection='row' alignItems='center' h='100%'>
-    <Button ref={btnRef} m='2' p='1' color='teal.400' backgroundColor='teal.800' onClick={onOpen}> <HamburgerIcon /></Button>
+    <Button ref={btnRef} m='2' p='1' color='teal.800' backgroundColor='white' onClick={onOpen}> <HamburgerIcon /></Button>
     <Link to="/"><Image boxSize='50px' objectFit='cover' borderRadius='full' color='gray' src={logo}></Image></Link>
       <Heading as="h1" p='2' fontSize="4xl">MiniSplitwise</Heading>
       <Spacer/>
-      <Button as={Link} to="/register" m='2' >Sign-Up</Button>
-      <Button as={Link} to="/login" m='2' >Login</Button>
+      {
+        user===null || user===undefined?<>
+        <Button as={Link} to="/register" m='2' >Sign-Up</Button>
+        <Button as={Link} to="/login" m='2' >Login</Button>
+        </>:<>
+        <Text fontWeight='bold' color="white">{user!==null?user.split("@")[0]:""}</Text>
+        <Button m='2' onClick={handleLogout}>Logout</Button>
+        </>
+      }
 
     </Box>
     </>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,12 +5,17 @@ import { store } from "./store";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+    <ChakraProvider>
+      <Router>
+        <App />
+      </Router>
+      </ChakraProvider>
     </Provider>
   </React.StrictMode>
 );
