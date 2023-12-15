6b577d0893e4894a9e113b2b1c1890d6b6760488
Chinthan23 <77500564+Chinthan23@users.noreply.github.com>
2023-12-15 14:43:12 +0530

    expense tracker update and total added

--- frontend/src/components/ExpenseTracker.js
+++ frontend/src/components/ExpenseTracker.js
@@ -1,10 +1,20 @@
 import {React,useState,useEffect} from 'react';
-import { Box, Heading, Grid, Text, Button, InputGroup, Input, Card,CardFooter,CardHeader, IconButton } from '@chakra-ui/react';
+import { Box, Heading, Grid, Text, Button, InputGroup, Input, Card,CardFooter,CardHeader, IconButton,Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalFooter, FormControl, FormLabel } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
 import { SmallAddIcon,EditIcon,DeleteIcon } from '@chakra-ui/icons';
 
 function ExpenseList({ expenses, onEdit, onDelete }) {
+  const [selectedExpense, setSelectedExpense] = useState(null);
+
+  const openEditModal = (expense) => {
+    setSelectedExpense(expense);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedExpense(null);
+  };
   return (
+    <>
     <Grid gap={4} mb={4} templateColumns='repeat(3, 1fr)'>
       {expenses.map((expense) => (
         <Card key={expense.billId} bg="papayawhip" >
@@ -13,7 +23,7 @@ function ExpenseList({ expenses, onEdit, onDelete }) {
             {expense.billName}
           </Heading>
           <Text fontSize="sm">
-            {expense.amount}
+          &#8377; {expense.amount}
           </Text>
         </CardHeader>
         <CardFooter>
@@ -22,7 +32,7 @@ function ExpenseList({ expenses, onEdit, onDelete }) {
               icon={<EditIcon />}
               colorScheme="teal"
               size="sm"
-              onClick={() => onEdit(expense)}
+              onClick={() => openEditModal(expense)}
               alignSelf='flex-start'
               w="50%"
             />
@@ -40,11 +50,76 @@ function ExpenseList({ expenses, onEdit, onDelete }) {
       </Card>
       ))}
     </Grid>
+      {selectedExpense && (
+            <EditExpenseForm
+              initialExpense={selectedExpense}
+              isOpen={selectedExpense !== null}
+              onClose={handleCloseModal}
+              onSubmit={(updatedExpense) => {
+                onEdit(updatedExpense);
+              }}
+            />
+      )}
+      </>
   );
 }
+function EditExpenseForm({ initialExpense, isOpen, onClose, onSubmit }) {
+  const [name, setName] = useState(initialExpense.billName);
+  const [amount, setAmount] = useState(initialExpense.amount);
+
+  const handleNameChange = (event) => setName(event.target.value);
+  const handleAmountChange = (event) => setAmount(event.target.value);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmit({ billName: name, amount: amount, billId: initialExpense.billId }); // Add billId for update reference
+    
+    onClose();
+  };
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Edit Expense</ModalHeader>
+        <ModalBody>
+          <FormControl>
+            <FormLabel htmlFor="name">Name</FormLabel>
+            <Input
+              id="name"
+              type="text"
+              value={name}
+              onChange={handleNameChange}
+            />
+          </FormControl>
+          <FormControl mt={4}>
+            <FormLabel htmlFor="amount">Amount</FormLabel>
+            <InputGroup>
+              <Input
+                id="amount"
+                type="number"
+                value={amount}
+                onChange={handleAmountChange}
+              />
+            </InputGroup>
+          </FormControl>
+        </ModalBody>
+        <ModalFooter>
+          <Button colorScheme="gray" mr={3} onClick={onClose}>
+            Cancel
+          </Button>
+          <Button colorScheme="teal" type="submit" onClick={handleSubmit}>
+            Save
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
+
 function AddExpenseForm({ onSubmit }) {
   const [name, setName] = useState("");
-  const [amount, setAmount] = useState(0);
+  const [amount, setAmount] = useState(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -59,7 +134,7 @@ function AddExpenseForm({ onSubmit }) {
         <Input placeholder="Expense Name" borderColor="black" m="2" value={name} onChange={(e) => setName(e.target.value)} />
       </InputGroup>
       <InputGroup>
-        <Input type="number" placeholder="Amount" borderColor="gray" m="2" value={amount} onChange={(e) => setAmount(e.target.value)} />
+        &#8377;<Input type="number" placeholder="Amount in &#8377;" borderColor="gray" m="2" value={amount} onChange={(e) => setAmount(e.target.value)} />
       </InputGroup>
       <Button leftIcon={<SmallAddIcon/>} type="submit" colorScheme="teal" m="2" size="lg">Add Expense</Button>
     </Box>
@@ -69,6 +144,7 @@ function AddExpenseForm({ onSubmit }) {
 function ExpenseTracker({user,jwt}) {
   const [expenses, setExpenses] = useState([]); // Array to store expenses
   const navigate=useNavigate();
+  const [totalExpense, setTotal]=useState(0);
   // console.log(user)
   useEffect(()=>{
     if(user===null || user===undefined){
@@ -89,12 +165,28 @@ function ExpenseTracker({user,jwt}) {
       return response.json();
     })
     .then((data) => {
-      // console.log(data); // Log the response data here
+      console.log(data); // Log the response data here
       setExpenses(data);
     })
     .catch((err) => {
       console.error('Error:', err);
     });
+    await fetch(`http://localhost:8081/personalBills/getTotalExpense/${user}`,{
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${jwt}`
+      }
+    })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok.');
+      }
+      return response.json();
+    })
+    .then((data)=>{
+      // console.log(data);
+      setTotal(data);
+    })
     }
     fetchExpenses();
   },[user,navigate,jwt]);
@@ -111,7 +203,7 @@ function ExpenseTracker({user,jwt}) {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        Authorization: `Bearer ${jwt}` 
+        Authorization: ` ${jwt}` 
       },
       body: JSON.stringify(expenseData)
     })
@@ -135,15 +227,37 @@ function ExpenseTracker({user,jwt}) {
     if(ret===0) setExpenses([...expenses, {"billName": expenseData.billName, "amount": expenseData.amount, "billId": expenseData.billId}]);
   };
 
-  const handleEditExpense = (updatedExpense) => {
+  const handleEditExpense = async (updatedExpense) => {
     // Update specific expense in state
-    const updatedExpenses = expenses.map(expense => expense.id === updatedExpense.id ? updatedExpense : expense);
+    console.log(updatedExpense)
+    await fetch(`http://localhost:8081/personalBills/updateBillById/${updatedExpense.billId}`,{
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${jwt}` 
+      },
+      body: JSON.stringify(updatedExpense)
+    })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok.');
+      }
+      return response;
+    })
+    .then((data) => {
+      // console.log(data); // Log the response data here
+      const updatedExpenses = expenses.map(expense => expense.billId === updatedExpense.billId ? updatedExpense : expense);
     setExpenses(updatedExpenses);
+    })
+    .catch((err) => {
+      console.error('Error:', err);
+      alert("Couldn't update!")
+    });
   };
 
   const handleDeleteExpense = async(event) => {
     // Update state by filtering out deleted expense
-    // console.log(event)
+    console.log(event,jwt)
     await fetch(`http://localhost:8081/personalBills/deleteByBillId/${event}`,{
       method: 'DELETE',
       headers: {
@@ -171,7 +285,12 @@ function ExpenseTracker({user,jwt}) {
     <Box display="flex" flexDirection="row" flexGrow={1} mt="3" ms="10">
       {/* Expense list on the left */}
       <Box p={4} backgroundColor="papayawhite" boxShadow='lg' m='2' rounded="md" w="65%">
-        <Heading as="h2">Your Expenses</Heading>
+        <Box display="flex" justifyContent="space-between">
+        <Box alignSelf="flex-start">
+          <Heading as="h2">Your Expenses</Heading>
+        </Box>
+        <Text>Total: &#8377;{totalExpense}</Text>
+      </Box>
         <ExpenseList expenses={expenses} onEdit={handleEditExpense} onDelete={handleDeleteExpense} />
       </Box>
       {/* Add expense form on the right */}
